8d70354d00ae1e9079c138f1156694cdeb5e8a43
Max Base <MaxBaseCode@Gmail.Com>
2023-04-08 07:27:10 +0100

    Create build.py

---
+++ .instances/build.py
@@ -0,0 +1,26 @@
+# ================================ CONFIG ================================
+VARIABLES = [
+  {
+    "Key": "Primary Color",
+    "File": "
+    "Regex": "",
+  },
+  {
+    "Key": "Secondly Color",
+    "File": "
+    "Regex": "",
+  },
+  {
+    "Key": "Full Name",
+    "File": "",
+    "Regex": "",
+  },
+  {
+    "Key": "App Name",
+    "File": "",
+    "Regex": "",
+  },
+]
+
+# ================================ INIT ================================
+# TODO
